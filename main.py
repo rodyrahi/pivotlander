ce0dc40c49baffdd089e90000306716761f82a5b
Rajvendra Rahi <45394091+rodyrahi@users.noreply.github.com>
2026-03-13 20:46:44 +0530

    lander more things added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
@@ -8,6 +9,8 @@ templates = Jinja2Templates(directory="templates")
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/")
 def read_root(request: Request):
